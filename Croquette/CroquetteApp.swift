11413b6e8d62a43a247a17380cda6ac45523ff5c
Brian Chang <brian.chang@binus.ac.id>
2026-04-27 15:54:45 +0700

    Initial Commit

---
+++ Croquette/CroquetteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CroquetteApp.swift
+//  Croquette
+//
+//  Created by Brian Chang on 27/04/26.
+//
+
+import SwiftUI
+
+@main
+struct CroquetteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
