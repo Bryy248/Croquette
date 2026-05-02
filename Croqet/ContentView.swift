eb0c3d880a6789f1c84919b6a07b5081efd1e4cf
Brian Chang <brian.chang@binus.ac.id>
2026-05-02 16:21:01 +0700

    fix: project page logic

--- Croqet/ContentView.swift
+++ Croqet/ContentView.swift
@@ -7,7 +7,17 @@
 
 import SwiftUI
 
+// Temporary Model
+struct TemporaryProject: Identifiable {
+    let id = UUID()
+    var name: String
+}
+
 struct ContentView: View {
+    // Temporary
+    @State private var projects: [TemporaryProject] =  [TemporaryProject(name: "tapak")]
+    // []
+    
     var body: some View {
         ZStack() {
             // Background Color Layer
@@ -24,42 +34,51 @@ struct ContentView: View {
                 }
                 .frame(maxWidth: .infinity, alignment: .leading)
                 
-                
-                VStack(spacing: 16) {
+                VStack(alignment: .leading, spacing: 16) {
                     Text("Projects")
                         .bold()
                         .font(.system(size: 36))
-                    Text("No existing project.")
-                        .fontWeight(.semibold)
-                        .font(Font.system(size: 16))
-                        .foregroundStyle(.gray)
+                    
+                    if projects.isEmpty {
+                        Text("No existing project.")
+                            .fontWeight(.semibold)
+                            .font(Font.system(size: 16))
+                            .foregroundStyle(.gray)
+                    }
+                    else {
+                        VStack(alignment: .leading, spacing: 12) {
+                            ForEach(projects) { project in
+                                HistoryCard()
+                            }
+                        }
+                    }
                 }
                 .frame(maxWidth: .infinity, alignment: .leading)
                 
                 Spacer()
                 
-                VStack(spacing: 16) {
-                    Image(systemName: "star")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 100, height: 100)
-                    
-                    Text("What are you making?")
-                        .fontWeight(.semibold)
-                        .font(Font.system(size: 24))
-                        .foregroundStyle(.primary)
-                    
-                    CroqetButton(title: "Add Project") {
+                if projects.isEmpty {
+                    VStack(spacing: 16) {
+                        Image(systemName: "star")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                        
+                        Text("What are you making?")
+                            .fontWeight(.semibold)
+                            .font(Font.system(size: 24))
+                            .foregroundStyle(.primary)
+                        
+                        CroqetButton(title: "Add Project") {
+                            
+                        }
                     }
                 }
                 
                 Spacer()
-                
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
             .padding(16)
-            
-            
         }
         .frame(maxWidth: .infinity , maxHeight: .infinity)
         
